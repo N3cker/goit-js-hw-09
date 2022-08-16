6d3e0d7eb222bcd6cae0885bd7cd6bbd122a8b9c
N3cker <piaszuu@gmail.com>
2022-08-16 13:56:08 +0200

    finish colorSwitcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.body;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId;
+
+btnStart.addEventListener('click', () => {
+    intervalId = setInterval(() => {
+        body.style.background = getRandomHexColor();
+    }, 1000);
+    btnStart.disabled = true;
+})
+
+btnStop.addEventListener('click', () => {
+    clearInterval(intervalId);
+    btnStart.disabled = false;
+})
